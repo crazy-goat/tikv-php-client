e450020a763e3c27ecc133977e1c4962554c2253
Piotr Hałas <piotr.h@decodo.com>
2026-03-30 13:17:31 +0200

    feat(txnkv): add TxnKvClient skeleton

---
+++ src/Client/TxnKv/TxnKvClient.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CrazyGoat\TiKV\Client\TxnKv;
+
+use CrazyGoat\TiKV\Client\Cache\RegionCache;
+use CrazyGoat\TiKV\Client\Cache\RegionCacheInterface;
+use CrazyGoat\TiKV\Client\Connection\PdClientInterface;
+use CrazyGoat\TiKV\Client\Grpc\GrpcClientInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+
+final class TxnKvClient
+{
+    public function __construct(
+        private readonly PdClientInterface $pdClient,
+        private readonly GrpcClientInterface $grpc,
+        private readonly RegionCacheInterface $regionCache = new RegionCache(),
+        private readonly int $maxBackoffMs = 20000,
+        private readonly LoggerInterface $logger = new NullLogger(),
+    ) {}
+
+    /**
+     * Begin a new transaction.
+     *
+     * @param array{
+     *   pessimistic?: bool,
+     *   priority?: int,
+     * } $options
+     */
+    public function begin(array $options = []): Transaction
+    {
+        $pessimistic = $options['pessimistic'] ?? true;
+        $priority = $options['priority'] ?? 0;
+
+        // TODO: Get timestamp from PD
+        $startTs = time() * 1000; // Placeholder
+
+        $txnId = uniqid('txn-', true);
+
+        $this->logger->info('Transaction started', [
+            'txnId' => $txnId,
+            'startTs' => $startTs,
+            'pessimistic' => $pessimistic,
+        ]);
+
+        return new Transaction(
+            txnId: $txnId,
+            startTs: $startTs,
+            pessimistic: $pessimistic,
+            priority: $priority,
+        );
+    }
+}
